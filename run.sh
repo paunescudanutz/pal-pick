56f76320b6656b14fefdc418bb8f07545febfda4
Paunescu Daniel <paunescudanutz@gmail.com>
2026-01-03 23:21:20 +0200

    organize project

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-rm app.exe
-# gcc *.c -lm -o app.exe -g; ./app.exe -h --pre "Vec3(" --post ")" --first-separator ", " --second-separator ", "
-gcc *.c -lm -o app.exe ; ./app.exe -x 1 --y 1 --width 50 --height 50 --first-separator "; " --second-separator "; " --label1 "R: " --label2 "G: " --label3 "B: "
+rm bin/palpick
+gcc src/*.c -lm -o bin/palpick ; ./bin/palpick -x 1 --y 1 --width 50 --height 50 --first-separator "; " --second-separator "; " --label1 "R: " --label2 "G: " --label3 "B: "
